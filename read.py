c22127c471211d7a7c8f6e6a3f5c57ee727c971d
Reeve Hou <sam2972570@gmail.com>
2023-08-23 00:35:59 +0800

    add list comprehension

--- read.py
+++ read.py
@@ -4,7 +4,22 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
 		count += 1
-		if count % 1000 ==0 :
+		if count % 10000 ==0 :
 			print(len(data))
 
-#　print(data[0])
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+
+#以下把有提到nice的留言收集起來
+
+NICE = []
+# for message in data:
+# 	if 'nice' in message:
+# 		NICE.append(message)
+# print('總共有', len(NICE), '則留言提到 nice')
+# print(NICE[0])
+
+
+# 上方可以改寫成下面快寫法
+NICE = [message for message in data if 'nice' in message]
+print('總共有', len(NICE), '則留言提到 nice')
+print(NICE[0])
